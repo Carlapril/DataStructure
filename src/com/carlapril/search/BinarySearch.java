cff497d2eff817d5d9a4486b21ae9d4d5928cc7b
Carlapril <carlapril@163.com>
2020-09-26 21:28:51 +0800

    just for practice

--- src/com/carlapril/search/BinarySearch.java
+++ src/com/carlapril/search/BinarySearch.java
@@ -1,6 +1,8 @@
 package com.carlapril.search;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author carlapril
@@ -9,35 +11,44 @@ import java.util.Arrays;
 //使用二分查找，确保数组是有序的！！
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {3,5,7,9,10,999};
-        int index = binarySearch(arr,0,arr.length-1,1000);
-        if (index==-1){
+        int[] arr = {3, 5, 7, 9, 10, 999, 1000};
+        List<Integer> resultIndex = binarySearch(arr, 0, arr.length - 1, 1000);
+        if (resultIndex == null) {
             System.out.println("未找到该数据");
-        }else {
-            System.out.println("index = " + index);
+        } else {
+            System.out.println("index = " + resultIndex);
         }
 
     }
 
     /**
-     *
-     * @param arr 数组
-     * @param left 数组左引索
-     * @param right 数组右引索
+     * @param arr     数组
+     * @param left    数组左引索
+     * @param right   数组右引索
      * @param findVal 需要查找的值
      * @return 返回查找到的值的下标，没有查到返回-1
      */
-    public static int binarySearch(int[] arr, int left,int right, int findVal){
-         int mid = (right+left)/2;
-         if(left>right){
-             return -1;
-         }
-         if (findVal> arr[mid]){//已知数组是升序
-             return binarySearch(arr, mid+1, right, findVal);
-         }else if (findVal<arr[mid]){
-             return binarySearch(arr, left, mid-1, findVal);
-        }else{
-             return mid;
-         }
+    public static List<Integer> binarySearch(int[] arr, int left, int right, int findVal) {
+        int mid = (right + left) / 2;
+        if (left > right) {
+            return new ArrayList<Integer>();
+        }
+        if (findVal > arr[mid]) {//已知数组是升序
+            return binarySearch(arr, mid + 1, right, findVal);
+        } else if (findVal < arr[mid]) {
+            return binarySearch(arr, left, mid - 1, findVal);
+        } else {
+            List<Integer> list = new ArrayList<Integer>();
+            int temp = mid;
+            while (true) {
+                if (temp < 0 || arr[temp] != findVal) {
+                    break;
+                }
+                list.add(temp);
+                temp--;
+            }
+            return list;
+
+        }
     }
 }
